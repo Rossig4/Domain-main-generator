ebc221ff980d596b4b103567a0f750921edf9795
Rossig4 <r.silvera.ag@gmail.com>
2021-06-17 17:30:16 +0000

    listo para entregar

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo from the console");
 };
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var ext = [".us", ".io", ".com", ".net"];
+var dominioP = document.getElementById("dominiosP");
+var dominio = [];
+for (let index = 0; index < pronoun.length; index++) {
+  const primeraParte = pronoun[index];
+  for (let index = 0; index < adj.length; index++) {
+    const segundaParte = adj[index];
+    for (let index = 0; index < noun.length; index++) {
+      const terceraParte = noun[index];
+      for (let index = 0; index < ext.length; index++) {
+        const cuartaParte = ext[index];
+        dominio.push(primeraParte + segundaParte + terceraParte + cuartaParte);
+        dominioP.innerHTML +=
+          primeraParte + segundaParte + terceraParte + cuartaParte + "<br>";
+      }
+    }
+  }
+}
+console.log(dominio);
